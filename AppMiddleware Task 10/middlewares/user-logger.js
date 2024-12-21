9862ed786b439a3a1d1949168b71644930705518
Rill <160830457+chappuis13@users.noreply.github.com>
2024-12-21 19:58:37 +0700

    Delete AppMiddleware Task 10 directory

--- AppMiddleware Task 10/middlewares/user-logger.js
+++
@@ -1,7 +0,0 @@
-const userLogger = (req, res, next) => {
-    const d = new Date().toISOString()
-    console.log(`[${d}] ${req.method} ${req.originalUrl}`);
-    next();
-}
-
-module.exports = userLogger
