84d85a50ca3ebdf053b6295e9bc47ea526ec1d7b
Rill <160830457+chappuis13@users.noreply.github.com>
2024-12-21 19:58:18 +0700

    Delete UserProfile Task 9 directory

--- UserProfile Task 9/delete-user.js
+++
@@ -1,8 +0,0 @@
-const {response} = require("express");
-
-fetch('http://localhost:3000/api/users/1', {
-    method: 'DELETE',
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.log(error))
